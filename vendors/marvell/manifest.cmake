2baf9baba303f792e4f24536adcda732f9c01704
Jason Hung <jhung@globalscaletechnologies.com>
2019-11-25 11:41:12 +0800

    vendors: marvell: add aw-cu345 iot development kit
    add globalscale iot development kit, the board is based on aw-cu345
    module (MW320 + DA14585).

--- vendors/marvell/manifest.cmake
+++ vendors/marvell/manifest.cmake
@@ -2,9 +2,11 @@ set(
     AFR_MANIFEST_SUPPORTED_BOARDS
     mw320
     mw322
+    cu345
     CACHE INTERNAL "Supported boards list."
 )
 
 set(AFR_MANIFEST_BOARD_DIR "boards")
 set(AFR_MANIFEST_BOARD_DIR_mw320 "boards/mw300_rd")
 set(AFR_MANIFEST_BOARD_DIR_mw322 "boards/mw300_rd")
+set(AFR_MANIFEST_BOARD_DIR_cu345 "boards/cu345_iot_dev_kit")
