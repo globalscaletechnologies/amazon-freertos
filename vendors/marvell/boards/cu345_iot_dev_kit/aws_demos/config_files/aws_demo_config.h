985dd86345fc6918b333e04711d6905032125f78
Jason Hung <jhung@globalscaletechnologies.com>
2019-12-19 11:35:12 +0800

    cu345: demo: sensor: Add aws iot senser demo function
    Add aws iot sensor demo function, the demo code currently supports
    below three sensors.
    
     - Si7021 humidity and temperature sensor
     - APDS-9301 ambient light Sensor
     - BMP180 digital pressure sensor

--- vendors/marvell/boards/cu345_iot_dev_kit/aws_demos/config_files/aws_demo_config.h
+++ vendors/marvell/boards/cu345_iot_dev_kit/aws_demos/config_files/aws_demo_config.h
@@ -99,7 +99,64 @@ to perform a TLS negotiation. */
 #define democonfigTCP_ECHO_TASKS_SEPARATE_TASK_STACK_SIZE  ( configMINIMAL_STACK_SIZE * 4 )
 #define democonfigTCP_ECHO_TASKS_SEPARATE_TASK_PRIORITY    ( tskIDLE_PRIORITY )
 
+/* Sensor demo task parameters. */
+#define democonfigSENSOR_DEMO_TASK_STACK_SIZE              ( configMINIMAL_STACK_SIZE * 4 )
+#define democonfigSENSOR_DEMO_TASK_PRIORITY                ( tskIDLE_PRIORITY )
+
 /* Send AWS IoT MQTT traffic encrypted to destination port 443. */
 #define democonfigMQTT_AGENT_CONNECT_FLAGS          	   ( mqttagentREQUIRE_TLS )
+
+/* AWS Iot Sensor Demo */
+#define democonfigDEMO_POLLING_INTERVAL                     60000
+
+/* Run custom demo runner for customization app */
+#define CUSTOM_DEMO_RUNNER_ENABLED                          ( 1 )
+
+#ifdef CUSTOM_DEMO_RUNNER_ENABLED
+
+/* To run a particular demo you need to define one of these.
+ * Only one demo can be configured at a time
+ *
+ *          CONFIG_SENSOR_DEMO_ENABLED
+ */
+
+#define CONFIG_SENSOR_DEMO_ENABLED
+
+#if defined( CONFIG_SENSOR_DEMO_ENABLED )
+    #define CUSTDEMO_entryFUNCTION              RunSensorDemo
+    #if defined( democonfigSENSOR_DEMO_TASK_STACK_SIZE )
+        #undef democonfigDEMO_STACKSIZE
+        #define democonfigDEMO_STACKSIZE    democonfigSENSOR_DEMO_TASK_STACK_SIZE
+    #endif
+    #if defined( democonfigSENSOR_DEMO_TASK_PRIORITY )
+        #undef democonfigDEMO_PRIORITY
+        #define democonfigDEMO_PRIORITY     democonfigSENSOR_DEMO_TASK_PRIORITY
+    #endif
+#endif
+
+#ifdef CONFIG_SENSOR_DEMO_ENABLED
+/* To run a particular sensor you need to define one of these.
+ * Only one sensor can be configured at a time
+ *
+ *  SENSOR_HUMIDITY_TEMPERATURE_SI7021
+ *  SENSOR_AMBIENT_LIGHT_APDS9301
+ *  SENSOR_DIGITAL_PRESSURE_BMP180
+ */
+    #define SENSOR_HUMIDITY_TEMPERATURE_SI7021
+
+    #ifdef SENSOR_HUMIDITY_TEMPERATURE_SI7021
+    #endif
+
+    #ifdef SENSOR_AMBIENT_LIGHT_APDS9301
+    #include <io_expander.h>
+    #define democonfigSENSOR_INT_GPIO                      (GPIO_IOP_N(5))
+    #define democonfigSENSOR_I2C_ADDR                      (0x39)
+    #endif
+
+    #ifdef SENSOR_DIGITAL_PRESSURE_BMP180
+    #endif
+#endif
+
+#endif /* CUSTOM_DEMO_RUNNER_ENABLED */
 
 #endif /* _AWS_DEMO_CONFIG_H_ */
