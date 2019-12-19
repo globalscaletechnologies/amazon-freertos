69ae273c057aa69d737164fef4b37df0b6146713
Jason Hung <jhung@globalscaletechnologies.com>
2019-12-19 09:54:15 +0800

    marvell: io_expander: add pcf8574 and pca9539 support
    io expander driver add pcf8574 and pca9539 chip support

--- vendors/marvell/WMSDK/mw320/sdk/src/drivers/peripherals/io_expander.c
+++ vendors/marvell/WMSDK/mw320/sdk/src/drivers/peripherals/io_expander.c
@@ -6,8 +6,24 @@
 #include <wm_os.h>
 #include <mdev_gpio.h>
 #include <io_expander.h>
+#include <pcf8574_expander_wrapper.h>
+#include <pca9539_expander_wrapper.h>
 
 int add_io_expander(gpio_expander_t **ioexp, int start)
 {
-	return -WM_FAIL;
+	int result = WM_SUCCESS;
+	int id = board_io_expander_id();
+
+	switch (id) {
+		case PCF8574_IDNUM:
+		result = add_pcf8574_io_expander(ioexp, start);
+		break;
+		case PCA9539_IDNUM:
+		result = add_pca9539_io_expander(ioexp, start);
+		break;
+		default:
+		result = -WM_FAIL;
+		break;
+	}
+	return result;
 }
