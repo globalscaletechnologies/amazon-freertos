1b5f0c1fb9613022525f34382275650e4c00e0d5
Jason Hung <jhung@globalscaletechnologies.com>
2019-12-24 08:50:23 +0800

    marvell: cu345: demo: disable verbose log message
    disable verbose log message

--- vendors/marvell/boards/cu345_iot_dev_kit/aws_demos/config_files/iot_config.h
+++ vendors/marvell/boards/cu345_iot_dev_kit/aws_demos/config_files/iot_config.h
@@ -43,12 +43,12 @@
 #define IOT_LOG_LEVEL_GLOBAL                    IOT_LOG_INFO
 #define IOT_LOG_LEVEL_DEMO                      IOT_LOG_INFO
 #define IOT_LOG_LEVEL_PLATFORM                  IOT_LOG_NONE
-#define IOT_LOG_LEVEL_NETWORK                   IOT_LOG_INFO
+#define IOT_LOG_LEVEL_NETWORK                   IOT_LOG_NONE
 #define IOT_LOG_LEVEL_TASKPOOL                  IOT_LOG_NONE
-#define IOT_LOG_LEVEL_MQTT                      IOT_LOG_INFO
-#define AWS_IOT_LOG_LEVEL_SHADOW                IOT_LOG_INFO
-#define AWS_IOT_LOG_LEVEL_DEFENDER              IOT_LOG_INFO
-#define IOT_LOG_LEVEL_HTTPS                     IOT_LOG_INFO
+#define IOT_LOG_LEVEL_MQTT                      IOT_LOG_NONE
+#define AWS_IOT_LOG_LEVEL_SHADOW                IOT_LOG_NONE
+#define AWS_IOT_LOG_LEVEL_DEFENDER              IOT_LOG_NONE
+#define IOT_LOG_LEVEL_HTTPS                     IOT_LOG_NONE
 
 /* Platform thread stack size and priority. */
 #define IOT_THREAD_DEFAULT_STACK_SIZE           3840
